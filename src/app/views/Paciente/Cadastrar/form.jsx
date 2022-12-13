d789f90a2b7e6229687de57aee8789d822258106
danielccosta9 <danielc.costa9@gmail.com>
2022-12-13 10:25:10 -0300

    Ajustes nos campos de cadastro de paciente

--- src/app/views/Paciente/Cadastrar/form.jsx
+++ src/app/views/Paciente/Cadastrar/form.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { IMaskInput } from 'react-imask';
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
 import { useState } from "react";
 import Axios from "axios";
 
 import {
+  Autocomplete,
   Button,
   Grid,
   Icon,
@@ -30,6 +27,16 @@ const button = {
   width: '200px',
   borderRadius: '15px',
 };
+
+const AutoComplete = styled(Autocomplete)(() => ({
+  marginBottom: '16px',
+}));
+
+const suggestions = [
+  { label: 'MASCULINO' },
+  { label: 'FEIMININO' },
+  { label: 'OUTROS' },
+];
 
 const InputCpf = React.forwardRef(function InputCpf(props, ref) {
   const { onChange, ...other } = props;
@@ -108,7 +115,7 @@ const SimpleForm = () => {
         setValues({});
       })
     alert('Cadastrado com sucesso!');
-    //window.location.reload(false);
+    window.location.reload(false);
   }
 
 
@@ -118,128 +125,97 @@ const SimpleForm = () => {
     setValues(values => ({ ...values, [name]: value }))
   }
 
-  const handleChangeMask = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
-
 
   return (
     <div>
       <ValidatorForm onSubmit={(event) => submit(event)}>
         <Grid container spacing={6}>
           <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
-
             <TextField
-              type="text"
+              required
+              id="nome"
               name="nome"
-              id="standard-basic"
+              label="Nome do Paciente"
+              fullWidth
               value={values.nome || ""}
               onChange={handleChange}
-              errorMessages={["Este campo é obrigatório"]}
-              label="Nome do Paciente"
-              validators={["required", "minStringLength: 3", "maxStringLength: 100"]}
+              errorMessages={"Nome inválido"}
+              validators={["required", "minStringLength: 3", "maxStringLength: 100", "matchRegexp:^[a-zA-ZÀ-ú ]*$"]}
             />
-
             <TextField
+              required
               type="text"
               name="cormobidade"
               id="standard-basic"
               value={values.cormobidade || ""}
               onChange={handleChange}
               errorMessages={["Este campo é obrigatório"]}
-              label="Comorbidades"
+              label="Cormobidade"
               validators={["required", "minStringLength: 3", "maxStringLength: 100"]}
             />
+
+            <AutoComplete
+              options={suggestions}
+              getOptionLabel={(option) => option.label}
+              renderInput={(params) => (
+                <TextField {...params} label="Sexo" variant="outlined" required />
+              )}
+              onChange={(event, value) => setValues(values => ({ ...values, sexo: value.label }))}
+            />
+
+          </Grid>
+          <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
+            <TextField
+              sx={{ width: '30ch' }}
+              required
+              id="cpf"
+              name="cpf"
+              label="CPF"
+              fullWidth
+              onChange={handleChange}
+              value={values.cpf || ''}
+              validators={["required", "minStringLength: 14", "maxStringLength: 14", "matchRegexp:^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$"]}
+              errorMessages={"CPF inválido"}
+              InputProps={{
+                inputComponent: InputCpf,
+              }}
+            />
+
 
             <TextField
-              type="text"
-              name="residencia"
-              id="standard-basic"
-              value={values.residencia || ""}
+              sx={{ width: '30ch' }}
+              required
+              id="telefone"
+              name="telefone"
+              label="Telefone"
+              fullWidth
+              onChange={handleChange}
+              value={values.telefone || ''}
+              validators={["required", "minStringLength: 15", "maxStringLength: 15"]}
+              errorMessages={"Telefone inválido"}
+              InputProps={{
+                inputComponent: InputPhone,
+              }}
+            />
+            <TextField
+              sx={{ width: '30ch' }}
+              required
+              type='date'
+              name="nascimento"
+              value={values.nascimento || ""}
               onChange={handleChange}
               errorMessages={["Este campo é obrigatório"]}
-              label="Bairro"
-              validators={["required", "minStringLength: 3", "maxStringLength: 100"]}
+              validators={["required"]}
+              id="formatted-text-mask-input"
             />
-
-          </Grid>
-
-          <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
-
-            <Box
-              sx={{
-                '& > :not(style)': {
-                  m: 1,
-                },
-              }}
-            >
-
-              <FormControl variant="standard">
-                <InputLabel htmlFor="formatted-text-mask-input">
-                  CPF
-                </InputLabel>
-                <Input
-                  name="cpf"
-                  value={values.cpf}
-                  onChange={handleChangeMask}
-                  id="formatted-text-mask-input"
-                  inputComponent={InputCpf}
-                  required={true}
-                />
-              </FormControl>
-            </Box>
-
-            <Box
-              sx={{
-                '& > :not(style)': {
-                  m: 1,
-                },
-              }}
-            >
-
-              <FormControl variant="standard">
-                <InputLabel htmlFor="formatted-text-mask-input">
-                  Data de Nascimento
-                </InputLabel>
-                <Input
-                  name="nascimento"
-                  value={values.nascimento}
-                  onChange={handleChangeMask}
-                  id="formatted-text-mask-input"
-                  inputComponent={InputDate}
-                  required={true}
-                />
-              </FormControl>
-            </Box>
-
-            <Box
-              sx={{
-                '& > :not(style)': {
-                  m: 1,
-                },
-              }}
-            >
-              <FormControl variant="standard">
-                <InputLabel htmlFor="formatted-text-mask-input">
-                  Telefone
-                </InputLabel>
-                <Input
-                  name="telefone"
-                  value={values.telefone}
-                  onChange={handleChangeMask}
-                  id="formatted-text-mask-input"
-                  inputComponent={InputPhone}
-                  required={true}
-                />
-              </FormControl>
-            </Box>
-
           </Grid>
         </Grid>
-        <Button color="primary" variant="contained" type="submit" style={button}>
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          style={button}
+        >
           <Icon>save</Icon>
           <Span sx={{ pl: 1, textTransform: "capitalize" }}>Salvar</Span>
         </Button>

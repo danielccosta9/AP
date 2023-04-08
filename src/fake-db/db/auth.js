fa50c9e6194f9c5f36ab32c28f0f5624d5040cab
danielccosta9 <danielc.costa9@gmail.com>
2023-04-08 17:29:15 -0300

    Aumentando dias de acesso ao sistema.

--- src/fake-db/db/auth.js
+++ src/fake-db/db/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import Mock from '../mock';
 
 const JWT_SECRET = 'jwt_secret_key';
-const JWT_VALIDITY = '1 days';
+const JWT_VALIDITY = '5 days';
 
 const userList = [
   {
